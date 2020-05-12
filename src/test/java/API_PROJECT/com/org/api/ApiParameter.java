e84b3e3383b549da15c49953a507c26cc95ffc88
praveenverma0803 <praveenverma0803@gmail.com>
2020-05-12 16:46:25 +0530

    updated API Parmeter class

--- src/test/java/API_PROJECT/com/org/api/ApiParameter.java
+++ src/test/java/API_PROJECT/com/org/api/ApiParameter.java
@@ -39,6 +39,6 @@ public class ApiParameter {
                 queryParam("appid", "439d4b804bc8187953eb36d2a8c26a02"). 
 			get(str).
 			then().
-			assertThat().statusCode(201);
+			assertThat().statusCode(200);
 		}
 }
